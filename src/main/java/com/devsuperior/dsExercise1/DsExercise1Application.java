d5bc14cf66186f1c0f8aed4d0dd6e316f06e3cb8
Leonardo A. de Souza <leo26gpl@gmail.com>
2021-01-15 04:06:54 -0300

    Initial Spring setup

---
+++ src/main/java/com/devsuperior/dsExercise1/DsExercise1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsExercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsExercise1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsExercise1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsExercise1/DsExercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsExercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsExercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
