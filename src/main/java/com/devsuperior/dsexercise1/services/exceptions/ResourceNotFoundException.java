c9ba88acea33be6bb9262ff1d753a1d1e7369060
Leonardo A. de Souza <leo26gpl@gmail.com>
2021-01-15 06:25:22 -0300

    Service Exceptions

---
+++ src/main/java/com/devsuperior/dsexercise1/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsexercise1.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
